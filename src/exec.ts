32199ac9e027f08d3ada6e80cb3a584bd5cf7492
yatatsu <yatatsukitagawa@gmail.com>
2020-09-01 17:04:02 +0900

    Fix handle output of `pod outdated` with shelljs.

--- src/exec.ts
+++ src/exec.ts
@@ -1,4 +1,5 @@
 import * as exec from "@actions/exec";
+import * as shelljs from "shelljs";
 
 export const execCatPodspec = async (path: string): Promise<string> => {
     const options: exec.ExecOptions = {};
@@ -20,20 +21,12 @@ export const execCatPodspec = async (path: string): Promise<string> => {
 };
 
 export const execPodOutdated = async (projectPath: string): Promise<string> => {
-    const options: exec.ExecOptions = {};
-    let output = "";
-    let error = "";
-    options.listeners = {
-        stdout: (data) => {
-            output += data.toString();
-        },
-        stderr: (data) => {
-            error += data.toString();
-        },
-    };
-    await exec.exec("pod outdated", [`--project-directory=${projectPath}`, "--no-ansi"]);
-    if (error || !output) {
-        throw Error("Fail to exec `pod outdated` " + error);
+    const temp = "temp.txt";
+    shelljs.exec(`pod outdated --project-directory=${projectPath} --no-ansi`).to(temp);
+    const result = shelljs.cat(temp);
+    shelljs.rm(temp);
+    if (result.stderr || !result.stdout) {
+        throw Error("Fail to exec `pod outdated` " + result.stderr);
     }
-    return output;
+    return result.stdout;
 };
